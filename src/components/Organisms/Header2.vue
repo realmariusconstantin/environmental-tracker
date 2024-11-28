d82fa88f38f506fd06aab37259a3548d3982c1d4
Bohdan <bodyaborzy@gmail.com>
2024-11-28 23:35:56 +0000

    homePage

---
+++ src/components/Organisms/Header2.vue
@@ -0,0 +1,882 @@
+
+
+<template>
+  <div class="header-buttons">
+    <div class="header-button-home">
+      <div class="header-button-home-child">
+      </div>
+      <div class="home">Home</div>
+    </div>
+    <div class="header-button-home1" id="headerButtonHome1">
+      <div class="impact-tracker">Impact tracker</div>
+    </div>
+    <div class="header-button-home2">
+      <div class="header-button-home-item">
+      </div>
+      <div class="activities">Activities</div>
+    </div>
+    <div class="header-button-home3">
+      <div class="header-button-home-item">
+      </div>
+      <div class="profile">Profile</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header2',
+};
+</script>
+<style >
+.firefly-post-apocalyptic-word {
+  position: absolute;
+  top: 125px;
+  left: 1018px;
+  width: 677px;
+  height: 798px;
+  object-fit: cover;
+}
+.track-your-environmental {
+  margin: 0;
+}
+.track-your-environmental-container {
+  position: absolute;
+  top: 315px;
+  left: 160px;
+  font-size: 96px;
+  display: inline-block;
+  width: 702px;
+  height: 345px;
+}
+.start {
+  position: relative;
+  line-height: 100%;
+}
+.start-button {
+  position: absolute;
+  top: 872px;
+  left: 160px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 40px;
+  border: 4px solid #fff;
+  box-sizing: border-box;
+  width: 316px;
+  height: 91px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 12px;
+  cursor: pointer;
+}
+.header-child {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  right: 0%;
+  bottom: 0%;
+  left: 0%;
+  background: #ffffff ; /*linear-gradient(180deg, #fff);*/
+}
+.header {
+  position: absolute;
+  top: -3px;
+  left: 0px;
+  width: 1920px;
+  height: 128px;
+}
+/*---------------------------------- img main ------------------------ */
+
+.image-container {
+  position: relative;
+  width:580px;
+  height: 800px;
+  /*clip-path: polygon(10% 0, 90% 0, 100% 100%, 0% 100%);*/
+  /* overflow: hidden;*/
+  transform: skewX(10deg);
+}
+
+.image-container img {
+  width: 100%;
+  height: 100%;
+  object-fit: fill;
+
+}
+
+
+/*---------------------------------- img main ------------------------ */
+.calculate-co2-emissions {
+  position: absolute;
+  top: 717px;
+  left: 160px;
+  font-size: 32px;
+  display: inline-block;
+  width: 847px;
+  height: 59px;
+}
+.sign-in {
+  position: absolute;
+  width: 50.99%;
+  top: 0%;
+  left: 0%;
+  display: inline-block;
+  cursor: pointer;
+}
+.log-in {
+  position: absolute;
+  width: 50.99%;
+  top: 0%;
+  left: 49.01%;
+  display: inline-block;
+  cursor: pointer;
+}
+.signinlogin {
+  position: absolute;
+  top: 40px;
+  left: 198px;
+  width: 355px;
+  height: 48px;
+  font-size: 40px;
+  color: #000;
+}
+.logo-removebg-preview-1-icon {
+  position: absolute;
+  top: 15px;
+  left: 28px;
+  width: 156px;
+  height: 100px;
+  object-fit: cover;
+}
+.users-rate-child {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  right: 0%;
+  bottom: 0%;
+  left: 0%;
+  background-color: none;
+}
+.rated-by-users {
+  position: absolute;
+  height: 37.5%;
+  width: 81.16%;
+  top: 51.25%;
+  left: 8.68%;
+  display: inline-block;
+}
+.users-rate-item {
+  position: absolute;
+  height: 46.75%;
+  width: 16.28%;
+  top: 4.4%;
+  right: 77.33%;
+  bottom: 48.85%;
+  left: 6.39%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+}
+.users-rate-inner {
+  position: absolute;
+  height: 46.75%;
+  width: 16.28%;
+  top: 4.4%;
+  right: 27.96%;
+  bottom: 48.85%;
+  left: 55.77%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+}
+.star-icon {
+  position: absolute;
+  height: 46.75%;
+  width: 16.28%;
+  top: 4.4%;
+  right: 44.8%;
+  bottom: 48.85%;
+  left: 38.92%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+}
+.users-rate-child1 {
+  position: absolute;
+  height: 46.75%;
+  width: 16.28%;
+  top: 4.4%;
+  right: 61.07%;
+  bottom: 48.85%;
+  left: 22.65%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+}
+.users-rate-child2 {
+  position: absolute;
+  height: 46.75%;
+  width: 16.28%;
+  top: 4.4%;
+  right: 9.95%;
+  bottom: 48.85%;
+  left: 73.77%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+}
+.users-rate {
+  position: absolute;
+  top: 877px;
+  left: 487px;
+  width: 230.4px;
+  height: 80px;
+}
+.header-button-home-child {
+  position: absolute;
+  height: 100%;
+  width: 99.75%;
+  top: 0%;
+  right: 0.25%;
+  bottom: 0%;
+  left: 0%;
+  background-color: #fff;
+}
+.home {
+  position: absolute;
+  height: 85%;
+  width: 85.39%;
+  top: 12.5%;
+  left: 14.61%;
+  display: inline-block;
+  background: none;
+}
+.header-button-home {
+  position: absolute;
+  height: 100%;
+  width: 30.74%;
+  top: -1.25%;
+  right: 58.71%;
+  bottom: 1.25%;
+  left: 10.55%;
+}
+.impact-tracker {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  left: 0%;
+  display: inline-block;
+}
+.header-button-home1 {
+  position: absolute;
+  height: 85%;
+  width: 56.25%;
+  top: 11.25%;
+  right: 0%;
+  bottom: 3.75%;
+  left: 43.75%;
+  cursor: pointer;
+}
+.header-button-home-item {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  right: 0%;
+  bottom: 0%;
+  left: 0%;
+  background-color: #fff;
+}
+.activities {
+  position: absolute;
+  height: 75%;
+  width: 93.81%;
+  top: 7.81%;
+  left: 6.22%;
+  display: inline-block;
+}
+.header-button-home2 {
+  position: absolute;
+  height: 100%;
+  width: 25.25%;
+  top: 0%;
+  right: 17.94%;
+  bottom: 0%;
+  left: 56.81%;
+  display: none;
+}
+.profile {
+  position: absolute;
+  height: 84.38%;
+  width: 90.52%;
+  top: 7.81%;
+  left: 9.49%;
+  display: inline-block;
+}
+.header-button-home3 {
+  position: absolute;
+  height: 100%;
+  width: 17.93%;
+  top: 0%;
+  right: 0%;
+  bottom: 0%;
+  left: 82.07%;
+  display: none;
+}
+.header-buttons {
+  position: absolute;
+  top: 25px;
+  left: 1322px;
+  width: 512px;
+  height: 80px;
+  font-size: 40px;
+  color: #000;
+}
+.home-page-prototype-s1 {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  background: linear-gradient(117.24deg, #4fb946 4.99%, #b046b9 91.81%);
+  width: 1920px;
+  height: 1080px;
+  overflow: hidden;
+  color: #fff;
+}
+.environmental-impact-services {
+  position: absolute;
+  top: 86px;
+  left: 462px;
+  font-size: 64px;
+  line-height: 100%;
+  display: inline-block;
+  width: 997px;
+  height: 84px;
+}
+.calculate-your-cars {
+  position: absolute;
+  top: 194px;
+  left: 418px;
+  font-size: 32px;
+  line-height: 100%;
+  text-align: center;
+  display: inline-block;
+  width: 1084px;
+  height: 64px;
+}
+.earth-5240543-1280-icon {
+  position: absolute;
+  top: 337px;
+  left: 92px;
+  border-radius: 30px;
+  width: 540px;
+  height: 400px;
+  object-fit: cover;
+}
+.beach-5697987-1280-icon {
+  position: absolute;
+  top: 337px;
+  left: 690px;
+  border-radius: 30px;
+  width: 540px;
+  height: 400px;
+  object-fit: cover;
+}
+.ai-generated-9085460-1280-icon {
+  position: absolute;
+  top: 337px;
+  left: 1288px;
+  border-radius: 30px;
+  width: 540px;
+  height: 400px;
+  object-fit: cover;
+}
+.co2-emission-tracker {
+  margin: 0;
+  font-size: 36px;
+}
+.co2-emission-tracker-container {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  line-height: 100%;
+  display: inline-block;
+  width: 467px;
+  height: 156px;
+}
+.text-box {
+  position: absolute;
+  top: 780px;
+  left: 92px;
+  width: 467px;
+  height: 156px;
+}
+.text-box1 {
+  position: absolute;
+  top: 779px;
+  left: 690px;
+  width: 467px;
+  height: 156px;
+}
+.join-our-community {
+  margin: 0;
+  font-size: 24px;
+}
+.text-box2 {
+  position: absolute;
+  top: 779px;
+  left: 1288px;
+  width: 467px;
+  height: 156px;
+  font-size: 36px;
+}
+.home-page-prototype-s2 {
+  position: absolute;
+  top: 1080px;
+  left: 0px;
+  border-radius: 2px;
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  width: 1920px;
+  height: 1080px;
+  overflow: hidden;
+}
+.eco-projects {
+  position: absolute;
+  top: 63px;
+  left: 747px;
+  font-size: 64px;
+  line-height: 100%;
+  display: inline-block;
+  width: 427px;
+  height: 76px;
+}
+.explore-initiatives-to {
+  position: absolute;
+  top: 175px;
+  left: 465px;
+  line-height: 100%;
+  display: inline-block;
+  width: 991px;
+  height: 70px;
+}
+.dsc04692-edit-icon {
+  position: absolute;
+  top: 280px;
+  left: 175px;
+  border-radius: 30px;
+  width: 700px;
+  height: 450px;
+  object-fit: cover;
+}
+.waterfall-6619377-1280-icon {
+  position: absolute;
+  top: 967px;
+  left: 175px;
+  border-radius: 30px;
+  width: 700px;
+  height: 450px;
+  object-fit: cover;
+}
+.namibia-2049203-1280-icon {
+  position: absolute;
+  top: 963px;
+  left: 1020px;
+  border-radius: 30px;
+  width: 700px;
+  height: 450px;
+  object-fit: cover;
+}
+.steering-wheel-7806891-1280-icon {
+  position: absolute;
+  top: 280px;
+  left: 1020px;
+  border-radius: 30px;
+  width: 700px;
+  height: 450px;
+  object-fit: cover;
+}
+.carbon-footprint {
+  margin: 0;
+  font-size: 40px;
+}
+.carbon-footprint-track-container {
+  position: absolute;
+  top: 784px;
+  left: 175px;
+  line-height: 100%;
+  display: inline-block;
+  width: 1021px;
+  height: 129px;
+}
+.discover-effective-ways {
+  margin: 0;
+  font-size: 32px;
+}
+.plastic-recycling-discover-container {
+  position: absolute;
+  top: 1457px;
+  left: 175px;
+  line-height: 100%;
+  display: inline-block;
+  width: 1032px;
+  font-size: 40px;
+}
+.learn-button {
+  position: absolute;
+  top: 792px;
+  left: 1404px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 40px;
+  border: 2px solid #000;
+  box-sizing: border-box;
+  width: 316px;
+  height: 91px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 12px;
+}
+.learn-button1 {
+  position: absolute;
+  top: 1492px;
+  left: 1404px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 40px;
+  border: 2px solid #000;
+  box-sizing: border-box;
+  width: 316px;
+  height: 91px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 12px;
+}
+.home-page-prototype-s3 {
+  position: absolute;
+  top: 2160px;
+  left: 0px;
+  border-radius: 2px;
+  background-color: #a8e6cf;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  width: 1920px;
+  height: 1638px;
+  overflow: hidden;
+  font-size: 32px;
+}
+.contact-us-reach-container {
+  position: absolute;
+  top: 81px;
+  left: 186px;
+  line-height: 100%;
+  display: inline-block;
+  width: 665px;
+  height: 207px;
+}
+.envelopes-6919301-1280-icon {
+  position: absolute;
+  top: 411px;
+  left: 157px;
+  border-radius: 30px;
+  width: 1607px;
+  height: 527px;
+  object-fit: cover;
+}
+.frame-child {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border-radius: 30px;
+  background-color: #fff;
+  border: 1px solid #000;
+  box-sizing: border-box;
+  width: 774px;
+  height: 717px;
+}
+.your-first-name {
+  position: absolute;
+  height: 36.89%;
+  width: 51.92%;
+  top: 0%;
+  left: 0%;
+  line-height: 100%;
+  display: inline-block;
+}
+.contact-us-form-field-child {
+  position: absolute;
+  height: 63.11%;
+  width: 100%;
+  top: 36.89%;
+  right: 0%;
+  bottom: 0%;
+  left: 0%;
+  border-radius: 20px;
+  background-color: #fff;
+  border: 1px solid #000;
+  box-sizing: border-box;
+}
+.enter-your-name {
+  position: absolute;
+  height: 31.07%;
+  width: 56.51%;
+  top: 53.4%;
+  left: 4.59%;
+  line-height: 100%;
+  color: #777;
+  display: inline-block;
+}
+.contact-us-form-field {
+  position: absolute;
+  top: 55px;
+  left: 55px;
+  width: 665px;
+  height: 103px;
+}
+.your-email-address {
+  position: absolute;
+  height: 36.89%;
+  width: 68.26%;
+  top: 0%;
+  left: 0%;
+  line-height: 100%;
+  display: inline-block;
+}
+.contact-us-form-field1 {
+  position: absolute;
+  top: 180px;
+  left: 55px;
+  width: 665px;
+  height: 103px;
+}
+.your-message {
+  position: absolute;
+  height: 24.23%;
+  width: 49.91%;
+  top: 0%;
+  left: 0%;
+  line-height: 100%;
+  display: inline-block;
+}
+.contact-us-form-field-inner {
+  position: absolute;
+  height: 82.99%;
+  width: 100%;
+  top: 19.07%;
+  right: 0%;
+  bottom: -2.06%;
+  left: 0%;
+  border-radius: 20px;
+  background-color: #fff;
+  border: 1px solid #000;
+  box-sizing: border-box;
+}
+.enter-your-message {
+  position: absolute;
+  height: 30.93%;
+  width: 66.78%;
+  top: 50%;
+  left: 4.59%;
+  line-height: 100%;
+  color: #777;
+  display: inline-block;
+}
+.contact-us-form-field2 {
+  position: absolute;
+  top: 305px;
+  left: 55px;
+  width: 665px;
+  height: 194px;
+}
+.submit {
+  width: 118px;
+  position: relative;
+  line-height: 100%;
+  display: inline-block;
+  flex-shrink: 0;
+}
+.x-icon {
+  width: 16px;
+  position: relative;
+  height: 16px;
+  overflow: hidden;
+  flex-shrink: 0;
+  display: none;
+}
+.learn-button2 {
+  position: absolute;
+  top: 570px;
+  left: 55px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 40px;
+  background-color: #388e3c;
+  width: 376px;
+  height: 91px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 12px;
+  box-sizing: border-box;
+  gap: 4px;
+  font-size: 32px;
+  color: #fff;
+}
+.rectangle-parent {
+  position: absolute;
+  top: 81px;
+  left: 1019px;
+  width: 774px;
+  height: 717px;
+  font-size: 24px;
+}
+.home-page-prototype-s4 {
+  position: absolute;
+  top: 3798px;
+  left: 0px;
+  border-radius: 2px;
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  width: 1920px;
+  height: 1080px;
+  overflow: hidden;
+  font-size: 64px;
+}
+.about-we-help-container {
+  position: absolute;
+  top: 84px;
+  left: 770px;
+  line-height: 100%;
+  display: inline-block;
+  width: 440px;
+  height: 286px;
+}
+.blank-line9 {
+  margin: 0;
+  font-size: 64px;
+}
+.join-join-us-container {
+  position: absolute;
+  top: 84px;
+  left: 139px;
+  line-height: 100%;
+  display: inline-block;
+  width: 440px;
+  height: 286px;
+}
+.contact-admin {
+  position: absolute;
+  top: 84px;
+  left: 1401px;
+  line-height: 100%;
+  display: inline-block;
+  width: 440px;
+  height: 231px;
+  cursor: pointer;
+  font-size: 40px;
+}
+.instagram-icon {
+  position: absolute;
+  height: 100%;
+  width: 14.16%;
+  top: 0%;
+  right: 85.84%;
+  bottom: 0%;
+  left: 0%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+}
+.twitter-icon {
+  position: absolute;
+  height: 100%;
+  width: 14.16%;
+  top: 0%;
+  right: 57.23%;
+  bottom: 0%;
+  left: 28.61%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+}
+.facebook-icon {
+  position: absolute;
+  height: 100%;
+  width: 14.16%;
+  top: 0%;
+  right: 28.61%;
+  bottom: 0%;
+  left: 57.23%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+}
+.linkedin-icon {
+  position: absolute;
+  height: 100%;
+  width: 14.16%;
+  top: 0%;
+  right: 0%;
+  bottom: 0%;
+  left: 85.84%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+}
+.footer-links {
+  position: absolute;
+  top: 167px;
+  left: 1452px;
+  width: 339px;
+  height: 48px;
+}
+.home-page-prototype-s5 {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border-radius: 2px;
+  background-color: #2f4f4f;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  width: 1920px;
+  height: 349px;
+  overflow: hidden;
+}
+.footer {
+  position: absolute;
+  top: 4878px;
+  left: 0px;
+  width: 1920px;
+  height: 349px;
+  text-align: center;
+  color: #fff;
+}
+
+.home-page-prototype-1 {
+  width: 100%;
+  position: relative;
+  height: 5227px;
+  text-align: left;
+  font-size: 24px;
+  color: #000;
+  font-family: Inter;
+}
+
+
+.contact-admin img{
+  padding: 10px 20px;
+  width: auto;
+  height: 60px;
+  margin: auto;
+}
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,24 +1,29 @@
 
 <template>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Home Page</title>
-  </head>
+
+
   <body>
-  <Navbar />
-  <h1>Welcome to the Home Page</h1>
+
+  <Header2 />
+  <HomeMain/>
+  <Footer/>
   </body>
-  </html>
+
 </template>
 
 <script>
-import Navbar from '@/components/Organisms/Navbar.vue'
+
+import Header2 from "@/components/Organisms/Header2.vue";
+import HomeMain from "@/components/Organisms/HomeMain.vue";
+import Footer from "@/components/Organisms/Footer.vue";
+
+
 
 export default {
   components: {
-    Navbar,
+    Header2,
+    HomeMain,
+    Footer,
   },
 }
 </script>
